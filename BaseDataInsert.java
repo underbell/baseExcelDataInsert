418b2c83f4042615c4279603100a8a78819b89a0
underbell <underbell.park@gmail.com>
2016-06-13 18:44:23 +0900

    new file:   BaseDataInsert.java new file:   README.md

---
+++ BaseDataInsert.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class BaseDataInsert	{
+	public static void main(String[] args) throws Exception {
+		String DB_URL = SystemValue.DB_URL;
+		String DB_USER = SystemValue.DB_URL;
+		String DB_PASSWORD = SystemValue.DB_PASSWORD;
+		String EXCEL_DATA_FORDER = SystemValue.EXCEL_DATA_FORDER;
+		
+		try {
+			// 드라이버를 로딩한다.
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e ) {
+			e.printStackTrace();
+		}
+
+        File processDir = new File(EXCEL_DATA_FORDER);
+		File[] fileList = processDir.listFiles();
+		
+		Connection conn = null;
+        
+		try{
+			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			conn.setAutoCommit(false);  
+			PreparedStatement pstmt = null;
+    			
+    		for(int i = 0 ; i < fileList.length ; i++)	{
+    			
+				FileInputStream inputStream = new FileInputStream(fileList[i]);
+				String tableName = fileList[i].getName();
+				tableName = tableName.substring(0, tableName.lastIndexOf("."));
+				System.out.println("___ tableName = " + tableName);
+				
+				ArrayList<String> column = new ArrayList<String>();
+				StringBuffer columnName = new StringBuffer();
+				StringBuffer prepareColumnData = new StringBuffer();
+				int j = 0;
+				
+		        Workbook workbook = new XSSFWorkbook(inputStream);
+		        Sheet firstSheet = workbook.getSheetAt(0);
+		        Iterator<Row> iterator = firstSheet.iterator();
+		         
+		        while (iterator.hasNext()) {
+		            Row nextRow = iterator.next();
+		            Iterator<Cell> cellIterator = nextRow.cellIterator();
+		            ArrayList<String> columnData = new ArrayList<String>();
+		            LocalDateTime currentTime = LocalDateTime.now();
+		            Timestamp t = Timestamp.valueOf(currentTime);
+		            while (cellIterator.hasNext()) {
+		                Cell cell = cellIterator.next();
+		                if(j == 0)	{
+		                	column.add(getCellValue(cell));
+		                	columnName.append(", ").append(getCellValue(cell));
+		                	prepareColumnData.append(", ?");
+		                }else	{
+		                	columnData.add(getCellValue(cell));
+		                }
+		            }
+		            
+		            if(j == 0)	{
+		            	pstmt = conn.prepareStatement("INSERT INTO " + tableName + " (" + columnName.substring(1) + " )" + " VALUES (" + prepareColumnData.substring(1) + " )");
+		            }else	{
+		            	for(int k = 0 ; k < column.size() ; k++)	{
+		            		String c_name = column.get(k);
+		            		String c_value = columnData.get(k);
+		            		if(c_name != null)	{
+		            			if(c_name.indexOf("CREATE_DATE") > -1 || c_name.indexOf("MODIFY_DATE") > -1)	{
+			            			pstmt.setTimestamp(k + 1, t);
+			            		}else 	{
+			            			pstmt.setString(k + 1, c_value);
+			            		}
+		            		}
+		            	}
+		            	 // addBatch에 담기
+		                pstmt.addBatch();
+		                // 파라미터 Clear
+		                pstmt.clearParameters() ;
+		            }
+		            j = 1;
+		        }
+		        
+		        // Batch 실행
+		        pstmt.executeBatch();
+				// Batch 초기화
+				pstmt.clearBatch();
+				conn.commit();
+				
+		        workbook.close();
+		        inputStream.close();
+    		}
+		}catch (Exception e) {
+			e.printStackTrace();
+			conn.rollback();
+		}finally	{
+			conn.commit();
+			conn.close();
+		}
+	}
+	
+	private static String getCellValue(Cell cell) {
+	    switch (cell.getCellType()) {
+	    case Cell.CELL_TYPE_STRING:
+	        return cell.getStringCellValue();
+	 
+	    case Cell.CELL_TYPE_BOOLEAN:
+	        return String.valueOf(cell.getBooleanCellValue());
+	 
+	    case Cell.CELL_TYPE_NUMERIC:
+	        return String.valueOf(cell.getNumericCellValue());
+	    }
+	 
+	    return "";
+	}
+}
+
