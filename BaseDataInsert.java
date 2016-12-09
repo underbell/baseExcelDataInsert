805cec1a0bb0c3667f2b44cfa9b17f813886c940
underbell <underbell.park@gmail.com>
2016-12-09 14:28:18 +0900

    date 처리

--- BaseDataInsert.java
+++ BaseDataInsert.java
@@ -6,7 +6,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -26,7 +28,7 @@ public class BaseDataInsert	{
 		String DB_USER = SystemValue.DB_USER;
 		String DB_PASSWORD = SystemValue.DB_PASSWORD;
 		String EXCEL_DATA_FORDER = SystemValue.EXCEL_DATA_FORDER;
-		
+
 		try {
 			// 드라이버를 로딩한다.
 			Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -36,30 +38,30 @@ public class BaseDataInsert	{
 
         File processDir = new File(EXCEL_DATA_FORDER);
 		File[] fileList = processDir.listFiles();
-		
+
 		Connection conn = null;
-        
+
 		try{
 			conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-			conn.setAutoCommit(false);  
+			conn.setAutoCommit(false);
 			PreparedStatement pstmt = null;
-    			
+
     		for(int i = 0 ; i < fileList.length ; i++)	{
-    			
+
 				FileInputStream inputStream = new FileInputStream(fileList[i]);
 				String tableName = fileList[i].getName();
 				tableName = tableName.substring(0, tableName.lastIndexOf("."));
 				System.out.println("___ tableName = " + tableName);
-				
+
 				ArrayList<String> column = new ArrayList<String>();
 				StringBuffer columnName = new StringBuffer();
 				StringBuffer prepareColumnData = new StringBuffer();
 				int j = 0;
-				
+
 		        Workbook workbook = new XSSFWorkbook(inputStream);
 		        Sheet firstSheet = workbook.getSheetAt(0);
 		        Iterator<Row> iterator = firstSheet.iterator();
-		         
+
 		        while (iterator.hasNext()) {
 		            Row nextRow = iterator.next();
 		            Iterator<Cell> cellIterator = nextRow.cellIterator();
@@ -76,7 +78,7 @@ public class BaseDataInsert	{
 		                	columnData.add(getCellValue(cell));
 		                }
 		            }
-		            
+
 		            if(j == 0)	{
 		            	pstmt = conn.prepareStatement("INSERT INTO " + tableName + " (" + columnName.substring(1) + " )" + " VALUES (" + prepareColumnData.substring(1) + " )");
 		            }else	{
@@ -86,6 +88,10 @@ public class BaseDataInsert	{
 		            		if(c_name != null)	{
 		            			if(c_name.indexOf("CREATE_DATE") > -1 || c_name.indexOf("MODIFY_DATE") > -1)	{
 			            			pstmt.setTimestamp(k + 1, t);
+			            		}else if(c_name.indexOf("DATE") > -1)	{
+			            			DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+			            			LocalDate date = LocalDate.parse(c_value, format);
+			            			pstmt.setDate(k + 1, java.sql.Date.valueOf(date));
 			            		}else 	{
 			            			pstmt.setString(k + 1, c_value);
 			            		}
@@ -98,13 +104,12 @@ public class BaseDataInsert	{
 		            }
 		            j = 1;
 		        }
-		        
 		        // Batch 실행
 		        pstmt.executeBatch();
 				// Batch 초기화
 				pstmt.clearBatch();
 				conn.commit();
-				
+
 		        workbook.close();
 		        inputStream.close();
     		}
@@ -120,19 +125,19 @@ public class BaseDataInsert	{
 			}
 		}
 	}
-	
+
 	private static String getCellValue(Cell cell) {
 	    switch (cell.getCellType()) {
 	    case Cell.CELL_TYPE_STRING:
 	        return cell.getStringCellValue();
-	 
+
 	    case Cell.CELL_TYPE_BOOLEAN:
 	        return String.valueOf(cell.getBooleanCellValue());
-	 
+
 	    case Cell.CELL_TYPE_NUMERIC:
 	        return String.valueOf(cell.getNumericCellValue());
 	    }
-	 
+
 	    return "";
 	}
 }
