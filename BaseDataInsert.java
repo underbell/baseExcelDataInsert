cb08a7c9ee377ac92d5f441ce78971351651f11c
underbell <underbell.park@gmail.com>
2016-12-19 15:20:10 +0900

    add date cloumn check

--- BaseDataInsert.java
+++ BaseDataInsert.java
@@ -1,10 +1,10 @@
-package test.data;
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FilenameFilter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -18,6 +18,9 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class BaseDataInsert	{
 	/**
 	 * @param args
@@ -28,6 +31,12 @@ public class BaseDataInsert	{
 		String DB_USER = SystemValue.DB_USER;
 		String DB_PASSWORD = SystemValue.DB_PASSWORD;
 		String EXCEL_DATA_FORDER = SystemValue.EXCEL_DATA_FORDER;
+		if(args != null && args.length == 4)	{
+			DB_URL = "jdbc:oracle:thin:@" + args[0] + ":1521:ORCL";
+			DB_USER = args[1];
+			DB_PASSWORD = args[2];
+			EXCEL_DATA_FORDER = args[3];
+		}
 
 		try {
 			// 드라이버를 로딩한다.
@@ -36,8 +45,20 @@ public class BaseDataInsert	{
 			e.printStackTrace();
 		}
 
-        File processDir = new File(EXCEL_DATA_FORDER);
-		File[] fileList = processDir.listFiles();
+    File processDir = new File(EXCEL_DATA_FORDER);
+		File[] fileList = processDir.listFiles(new FilenameFilter(){
+			@Override
+			public boolean accept(File dir, String name) {
+				if(name.lastIndexOf('.')>0)	{
+	                  int lastIndex = name.lastIndexOf('.');
+	                  String str = name.substring(lastIndex);
+	                  if(".xlsx".equals(str) && name.indexOf("TB_") > -1)	{
+	                     return true;
+	                  }
+	               }
+				return false;
+			}
+		});
 
 		Connection conn = null;
 
@@ -51,7 +72,21 @@ public class BaseDataInsert	{
 				FileInputStream inputStream = new FileInputStream(fileList[i]);
 				String tableName = fileList[i].getName();
 				tableName = tableName.substring(0, tableName.lastIndexOf("."));
-				System.out.println("___ tableName = " + tableName);
+				if(tableName.indexOf("TB_") > 0)	{
+					tableName = tableName.substring(tableName.indexOf("TB_"), tableName.length());
+				}
+				log.info("tableName = {}", tableName);
+
+				pstmt = conn.prepareStatement("SELECT COLUMN_NAME FROM USER_TAB_COLUMNS WHERE TABLE_NAME = '" + tableName + "' AND DATA_TYPE = 'DATE'");
+				ResultSet rs = pstmt.executeQuery();
+				ArrayList<String> dateColList = new ArrayList<>();
+				while(rs.next()) {
+				    String name = rs.getString(1);
+				    dateColList.add(name);
+				}
+				System.out.println(dateColList.size());
+				dateColList.stream().forEach(System.out::println);
+
 
 				ArrayList<String> column = new ArrayList<String>();
 				StringBuffer columnName = new StringBuffer();
@@ -85,13 +120,20 @@ public class BaseDataInsert	{
 		            		String c_name = column.get(k);
 		            		String c_value = columnData.get(k);
 		            		if(c_name != null)	{
-		            			if(c_name.indexOf("CREATE_DATE") > -1 || c_name.indexOf("MODIFY_DATE") > -1)	{
-			            			pstmt.setTimestamp(k + 1, t);
-			            		}else if(c_name.indexOf("DATE") > -1)	{
-			            			DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-			            			LocalDate date = LocalDate.parse(c_value, format);
-			            			pstmt.setDate(k + 1, java.sql.Date.valueOf(date));
-			            		}else 	{
+		            			if(dateColList.size() > 0 && dateColList.stream().anyMatch(p -> p.equals(c_name)))	{
+			            			if(c_name.indexOf("CREATE_DATE") > -1 || c_name.indexOf("MODIFY_DATE") > -1 || "CREATED".equals(c_name) || "UPDATED".equals(c_name))	{
+				            			pstmt.setTimestamp(k + 1, t);
+				            		}else if(c_name.indexOf("DATE") > -1 && c_value.length() >= 8)	{
+				            			DateTimeFormatter format;
+				            			if(c_value.length() == 8)	{
+				            				format = DateTimeFormatter.ofPattern("yyyyMMdd");
+				            			}else	{
+				            				format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+				            			}
+				            			LocalDate date = LocalDate.parse(c_value, format);
+				            			pstmt.setDate(k + 1, java.sql.Date.valueOf(date));
+				            		}
+		            			}else 	{
 			            			pstmt.setString(k + 1, c_value);
 			            		}
 		            		}
